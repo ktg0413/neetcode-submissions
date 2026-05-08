eaa9a2b026244178fb5c786da6db7ce90a52e66d
Taegyun Kim <kimteagyun0413@gmail.com>
2026-05-07 23:47:05 -0400

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut vec: Vec<char> = vec![];
+        let mut state: bool = false;
+        for ch in s.chars() {
+            if ch == '(' {
+                vec.push(ch); // (
+            } else if ch == '{' {
+                vec.push(ch); // {
+            } else if ch == '[' {
+                vec.push(ch); // [
+            }else if ch == ')' {
+                if vec.pop() == Some('(') {state = true;} else {return false;}
+            } else if ch == '}' {
+                if vec.pop() == Some('{') {state = true;} else {return false;}   
+            } else if ch == ']' {
+                if vec.pop() == Some('[') {state = true;} else {return false;}
+            }
+        } 
+        if vec.len() > 0 {state = false;}
+        state
+    }
+}
